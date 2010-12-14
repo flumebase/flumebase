fa7bcef99da424f7e67859cdfff3763573345e01
Aaron Kimball <akimball83@gmail.com>
2010-12-14 15:11:47 -0800

    Add WindowedHashMap for use in joins.

---
+++ src/main/java/com/odiago/rtengine/util/WindowedHashMap.java
@@ -0,0 +1,282 @@
+// (c) Copyright 2010 Odiago, Inc.
+
+package com.odiago.rtengine.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import com.cloudera.util.Pair;
+
+/**
+ * A hash map implementation suitable for windowed join operations.  Each
+ * entry in the HashMap has, in addition to a key and a value, a
+ * <i>timestamp</i> field with type T.
+ *
+ * <p>Entries are kept in a (k, List (t, v)) HashMap internally. There is a
+ * second TreeMap contained inside which maps (timestamp, List k). This allows
+ * for time-ranged iteration over the entries in the map, as well as insertion
+ * of timestamped entries out of order (which java.util.LinkedHashMap does not
+ * provide).  </p>
+ *
+ * <p>Multiple keys may have the same timestamp.</p>
+ * <p>This map may not store null values.</p>
+ */
+public class WindowedHashMap<K, V, T extends Comparable<T>>
+    implements Map<K, List<Pair<T, V>>> {
+  private HashMap<K, List<Pair<T, V>>> mHashMap;
+  private TreeMap<T, List<K>> mTimestamps;
+
+  public WindowedHashMap() {
+    mHashMap = new HashMap<K, List<Pair<T, V>>>();
+    mTimestamps = new TreeMap<T, List<K>>();
+  }
+
+  @Override
+  public int size() {
+    return mHashMap.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return mHashMap.isEmpty();
+  }
+
+  @Override
+  public boolean containsKey(Object key) {
+    return mHashMap.containsKey(key);
+  }
+
+  @Override
+  public boolean containsValue(Object value) {
+    return mHashMap.containsValue(value);
+  }
+
+  @Override
+  public List<Pair<T, V>> get(Object key) {
+    return mHashMap.get(key);
+  }
+
+  /**
+   * @return all values for the specified timestamp.
+   */
+  public List<V> getTimestamp(T timestamp) {
+    List<K> keysForTs = mTimestamps.get(timestamp);
+    if (null == keysForTs) {
+      return Collections.emptyList();
+    }
+
+    List<V> out = new ArrayList<V>();
+    for (K k : keysForTs) {
+      List<Pair<T, V>> timesAndValsForKey = mHashMap.get(k);
+      assert(timesAndValsForKey != null);
+      for (Pair<T, V> pr : timesAndValsForKey) {
+        out.add(pr.getRight());
+      }
+    }
+
+    return out;
+  }
+
+  /**
+   * Allows a multi-put of (timestamp, value) pairs with the same key.
+   */
+  @Override
+  public List<Pair<T, V>> put(K key, List<Pair<T, V>> timesAndVals) {
+    List<Pair<T, V>> old = mHashMap.get(key);
+    for (Pair<T, V> pr : timesAndVals) {
+      put(key, pr.getRight(), pr.getLeft());
+    }
+
+    return old;
+  }
+
+  public void put(K key, V value, T timestamp) {
+    Pair<T, V> timeVal = new Pair<T, V>(timestamp, value);
+    List<Pair<T, V>> lstForKey = mHashMap.get(key);
+    if (null == lstForKey) {
+      lstForKey = new ArrayList<Pair<T, V>>();
+      mHashMap.put(key, lstForKey);
+    }
+
+    lstForKey.add(timeVal);
+
+    List<K> lstForT = mTimestamps.get(timestamp);
+    if (null == lstForT) {
+      lstForT = new ArrayList<K>();
+      mTimestamps.put(timestamp, lstForT);
+    }
+    lstForT.add(key);
+  }
+
+  /**
+   * Removes the specified key from the map.
+   */
+  @Override
+  public List<Pair<T, V>> remove(Object key) {
+    List<Pair<T, V>> timesAndVals = mHashMap.remove(key);
+    if (null == timesAndVals) {
+      // If we couldn't find this key, break early.
+      return null;
+    }
+
+    // Remove all elements from the timestamp-driven map.
+    for (Pair<T, V> tv : timesAndVals) {
+      T t = tv.getLeft();
+      List<K> keysForTs = mTimestamps.get(t);
+      Iterator<K> it = keysForTs.iterator();
+      while (it.hasNext()) {
+        K someKey = it.next();
+        if (someKey.equals(key)) {
+          it.remove();
+        }
+      }
+
+      if (keysForTs.size() == 0) {
+        // Only key for the timestamp; remove the timestamp from the
+        // mapping entirely.
+        mTimestamps.remove(t);
+      }
+    }
+
+    return timesAndVals;
+  }
+
+  @Override
+  public void putAll(Map<? extends K,? extends List<Pair<T, V>>> m) {
+    for (Map.Entry<? extends K, ? extends List<Pair<T, V>>> entry : m.entrySet()) {
+      put(entry.getKey(), entry.getValue());
+    }
+  }
+
+  @Override
+  public void clear() {
+    mTimestamps.clear();
+    mHashMap.clear();
+  }
+
+  @Override
+  public Set<K> keySet() {
+    return mHashMap.keySet();
+  }
+
+  @Override
+  public Collection<List<Pair<T, V>>> values() {
+    return mHashMap.values();
+  }
+
+  @Override
+  public Set<Map.Entry<K,List<Pair<T, V>>>> entrySet() {
+    return mHashMap.entrySet();
+  }
+
+  @Override
+  public boolean equals(Object otherObj) {
+    if (null == otherObj) {
+      return false;
+    } else if (!otherObj.getClass().equals(getClass())) {
+      return false;
+    }
+
+    WindowedHashMap<K, V, T> other = (WindowedHashMap<K, V, T>) otherObj;
+    return mHashMap.equals(other.mHashMap) && mTimestamps.equals(other.mTimestamps);
+  }
+
+  @Override
+  public int hashCode() {
+    return mHashMap.hashCode() ^ mTimestamps.hashCode();
+  }
+
+  /**
+   * Look up all values for a key within a given timestamp range. 
+   * @returns a list of values such that m[key, t] = v and lo &lt;= t &lt; hi.
+   * Returns the empty list if no such key can be found within that window.
+   */
+  public List<V> getRange(K key, T lo, T hi) {
+    List<Pair<T, V>> timesAndVals = mHashMap.get(key);
+    
+    boolean first = true;
+    List<V> out = null;
+
+    if (null == timesAndVals) {
+      // No times/vals for that key.
+      return Collections.emptyList();
+    }
+
+    for (Pair<T, V> pr : timesAndVals) {
+      if (timeInRange(lo, pr.getLeft(), hi)) {
+        if (null == out) {
+          // Start by using a singleton list to hold this object. 
+          out = Collections.singletonList(pr.getRight());
+        } else if (first) {
+          // Promote to a linked list
+          out = new LinkedList<V>(out);
+          out.add(pr.getRight());
+          first = false;
+        } else {
+          // We're already in a linked list; just add it.
+          out.add(pr.getRight());
+        }
+      }
+    }
+
+    if (null == out) {
+      return Collections.emptyList();
+    }
+
+    return out;
+  }
+
+  /**
+   * @return true if lo &lt;= test &lt; hi.
+   */
+  private boolean timeInRange(T lo, T test, T hi) {
+    return lo.compareTo(test) <= 0 && test.compareTo(hi) < 0;
+  }
+
+  /**
+   * Remove all (k, v) pairs where the timestamp for the entry is less than
+   * the value specified by 'test'.
+   */
+  public void removeOlderThan(T test) {
+    // Get a view of all timestamps in the map less than 'test'.
+    Map<T, List<K>> subMap = mTimestamps.headMap(test, false);
+
+    Set<Map.Entry<T, List<K>>> entrySet = subMap.entrySet();
+    Iterator<Map.Entry<T, List<K>>> entries = entrySet.iterator();
+    while (entries.hasNext()) {
+      Map.Entry<T, List<K>> entry = entries.next();
+      T removeTime = entry.getKey();
+      List<K> removeKeys = entry.getValue();
+
+      // Remove all (t, v) pairs from mHashMap(k) for all k in removeKeys,
+      // where t = removeTime. If the list at mHashMap(k) is empty, remove
+      // mHashMap(k).
+      for (K removeKey : removeKeys) {
+        List<Pair<T, V>> pairs = mHashMap.get(removeKey);
+        Iterator<Pair<T, V>> pairIter = pairs.iterator();
+        while (pairIter.hasNext()) {
+          Pair<T, V> pr = pairIter.next();
+          if (pr.getLeft().equals(removeTime)) {
+            pairIter.remove();
+          }
+        }
+
+        if (pairs.size() == 0) {
+          // Remove the entire key/list from mHashMap.
+          mHashMap.remove(removeKey);
+        }
+      }
+
+      // Remove the entry for this timestamp from the underlying map.
+      entries.remove();
+    }
+  }
+}
---
+++ src/test/java/com/odiago/rtengine/util/TestWindowedHashMap.java
@@ -0,0 +1,137 @@
+// (c) Copyright 2010 Odiago, Inc.
+
+package com.odiago.rtengine.util;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.cloudera.util.Pair;
+
+/**
+ * Test that the WindowedHashMap provides the operations we expect.
+ */
+public class TestWindowedHashMap {
+  private <T> void assertContains(T obj, Iterable<T> things) {
+    for (T thing : things) {
+      if (obj.equals(thing)) {
+        return; // Found it.
+      }
+    }
+
+    throw new AssertionError("Could not find " + obj + " in " + things);
+  }
+
+  @Test
+  public void testMap() {
+    // Map with string keys, integer values, and long timestamps.
+    WindowedHashMap<String, Integer, Long> map = new WindowedHashMap<String, Integer, Long>();
+
+    // Put an entry in the map.
+    map.put("foo", Integer.valueOf(42), Long.valueOf(312));
+
+    // Verify that we can retrieve it by unrefined key.
+    List<Pair<Long, Integer>> vals = map.get("foo");
+    assertNotNull(vals);
+    assertEquals(1, vals.size());
+    assertEquals(Integer.valueOf(42), vals.get(0).getRight());
+    assertEquals(Long.valueOf(312), vals.get(0).getLeft());
+
+    // Verify that we can retrieve it by key in time range.
+    List<Integer> refined = map.getRange("foo", Long.valueOf(300), Long.valueOf(400));
+    assertNotNull(refined);
+    assertEquals(1, refined.size());
+    assertEquals(Integer.valueOf(42), refined.get(0));
+
+    // Verify that time ranges before, after, and immediately following it do
+    // not include the value.
+    refined = map.getRange("foo", Long.valueOf(200), Long.valueOf(300));
+    assertEquals(0, refined.size());
+    refined = map.getRange("foo", Long.valueOf(500), Long.valueOf(600));
+    assertEquals(0, refined.size());
+    refined = map.getRange("foo", Long.valueOf(300), Long.valueOf(312));
+    assertEquals(0, refined.size());
+
+    // This side of the interval should return a value though.
+    refined = map.getRange("foo", Long.valueOf(312), Long.valueOf(315));
+    assertEquals(1, refined.size());
+    assertEquals(Integer.valueOf(42), refined.get(0));
+
+    // Verify that the correct time range but the wrong key does not include
+    // it.
+    refined = map.getRange("bar", Long.valueOf(300), Long.valueOf(400));
+    assertEquals(0, refined.size());
+
+    // Add another key with a new timestamp.
+    map.put("bar", Integer.valueOf(43), Long.valueOf(611));
+    // Verify that we can retrieve it.
+    refined = map.getRange("bar", Long.valueOf(600), Long.valueOf(700));
+    assertEquals(1, refined.size());
+    assertEquals(Integer.valueOf(43), refined.get(0));
+
+    // Add another key with the first timestamp.
+    map.put("baz", Integer.valueOf(44), Long.valueOf(312));
+    // Looking up values by timestamp should return both.
+    List<Integer> valsForT = map.getTimestamp(Long.valueOf(312));
+    assertNotNull(valsForT);
+    assertEquals(2, valsForT.size());
+    assertContains(Integer.valueOf(42), valsForT);
+    assertContains(Integer.valueOf(44), valsForT);
+
+    // Looking up 'foo' should still work.
+    refined = map.getRange("foo", Long.valueOf(300), Long.valueOf(400));
+    assertNotNull(refined);
+    assertEquals(1, refined.size());
+    assertEquals(Integer.valueOf(42), refined.get(0));
+
+    // Remove everything older than 312.
+    map.removeOlderThan(Long.valueOf(312));
+    // Verify that our original foo entry is still there.
+    refined = map.getRange("foo", Long.valueOf(300), Long.valueOf(400));
+    assertNotNull(refined);
+    assertEquals(1, refined.size());
+    assertEquals(Integer.valueOf(42), refined.get(0));
+
+    // Remove the older entries by expiring them.
+    map.removeOlderThan(Long.valueOf(500));
+    // Try to look it up again, it should be gone.
+    refined = map.getRange("foo", Long.valueOf(300), Long.valueOf(400));
+    assertNotNull(refined);
+    assertEquals(0, refined.size());
+
+    refined = map.getRange("baz", Long.valueOf(300), Long.valueOf(400));
+    assertNotNull(refined);
+    assertEquals(0, refined.size());
+
+    // Now remove "bar" and assert that everything is empty.
+    WindowedHashMap<String, Integer, Long> emptyMap =
+        new WindowedHashMap<String, Integer, Long>();
+    map.remove("bar");
+    assertEquals(emptyMap, map);
+
+    // Put three entries for 'foo' in at different times, retrieve them in
+    // various ways.
+    map.put("foo", Integer.valueOf(1), Long.valueOf(1000));
+    map.put("foo", Integer.valueOf(2), Long.valueOf(2000));
+    map.put("foo", Integer.valueOf(3), Long.valueOf(3000));
+    
+    refined = map.getRange("foo", Long.valueOf(1500), Long.valueOf(3500));
+    assertEquals(2, refined.size());
+    assertContains(Integer.valueOf(2), refined);
+    assertContains(Integer.valueOf(3), refined);
+    
+    refined = map.getRange("foo", Long.valueOf(500), Long.valueOf(3500));
+    assertEquals(3, refined.size());
+    assertContains(Integer.valueOf(1), refined);
+    assertContains(Integer.valueOf(2), refined);
+    assertContains(Integer.valueOf(3), refined);
+
+    map.removeOlderThan(Long.valueOf(2200));
+    refined = map.getRange("foo", Long.valueOf(500), Long.valueOf(3500));
+    assertEquals(1, refined.size());
+    assertContains(Integer.valueOf(3), refined);
+  }
+}
+
